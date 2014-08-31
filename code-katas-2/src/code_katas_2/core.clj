e2a305ba5015ba6dc7520f7595d814bbe4594a9c
Daniel <dantomramos@gmail.com>
2014-08-31 19:05:06 -0300

    Se termina la funcion unpartial, en este caso nos basamos en la documentacion del clojure para el let, recur y partial. Tambien revizamos las ppt del curso para enteder mas a fondo el let.

--- code-katas-2/src/code_katas_2/core.clj
+++ code-katas-2/src/code_katas_2/core.clj
@@ -1,18 +1,20 @@
 (ns code-katas-2.core)
-
-
+ 
 (defn unpartial
   "Escribir una funcion que acepte una funcion parcial con cantidad de argumentos desconocida,
    retornar una funcion equivalente de n argumentos"
-  ;en este ejercicio creo que debemos utilizar la funcion partial.
+  ;Documentacion utilizada para el primer ejercicio
   ;la documentacion del partial http://clojuredocs.org/clojure_core/clojure.core/partial 
-  ;estudiando las ppt del curso vemos que podemos utilizar la funcion let http://clojuredocs.org/clojure_core/clojure.core/let
-  
-  [f]
-  (partial(fn [f & args] ))
-  
-
-           )
+  ;estudiando las ppt del curso vemos que podemos utilizar la funcion let, http://clojuredocs.org/clojure_core/clojure.core/let  
+  ;la documentacion del recur http://clojuredocs.org/clojure_core/clojure.core/recur
+   (partial
+     (fn [f & args];aqui formamos la lista f mas los argumentos 
+     (let [q(f (first args))] ; el let "guarda" en "q" f mas el primer argumento de f.
+     (if (fn? q); en este if preguntamos si "q" es es una funcion(en este caso si no esta vacia) luego del let
+     (recur q (rest args)); aqui llamamos recursivamente a con "q" y el resto de los argumentos de f.
+     q)))
+     f)
+  )
 
 
 (defn search
